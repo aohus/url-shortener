7451bb0be8a4f59a8e7bc1db38990f567ef72bbb
aohus <dbtnghk528@gmail.com>
2024-06-20 00:40:54 +0900

    feature: update unittest for service

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,99 +1,81 @@
-import pytest
-from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
+# import pytest
 
-from .main import app, get_session
-from .model import URL
-from .orm import init_db
-from .repository import URLRepository
-from .service import URLService
+# # app
+# from adapters.orm import metadata
+# from adapters.repository import SqlAlchemyRepository
+# from domain import model
+# from domain.model import URL
+# from fastapi.testclient import TestClient
+# from main import app
+# from service_layer import services
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
 
-# 테스트용 데이터베이스 설정
-DATABASE_URL_TEST = "sqlite:///./test_url_shortener.db"
-engine_test = create_engine(DATABASE_URL_TEST)
-
-
-# 테스트용 세션 생성
-def get_test_session():
-    with Session(engine_test) as session:
-        yield session
-
-
-# 애플리케이션 인스턴스 생성
-client = TestClient(app)
+# # 테스트용 데이터베이스 설정
+# DATABASE_URL_TEST = "sqlite:///./test_url_shortener.db"
+# engine_test = create_engine(
+#     DATABASE_URL_TEST,
+#     connect_args={"check_same_thread": False},
+# )
 
 
-# 데이터베이스 초기화 함수
-@pytest.fixture(autouse=True)
-def setup_and_teardown():
-    SQLModel.metadata.create_all(engine_test)
-    yield
-    SQLModel.metadata.drop_all(engine_test)
+# # 테스트용 세션 생성
+# get_test_session = sessionmaker(bind=engine_test)
 
 
-# 종속성 주입 함수 대체
-app.dependency_overrides[get_session] = get_test_session
+# # 애플리케이션 인스턴스 생성
+# client = TestClient(app)
 
 
-# 단축 URL 생성 테스트
-def test_create_short_url():
-    response = client.post("/shorten", json={"url": "https://www.example.com"})
-    assert response.status_code == 200
-    data = response.json()
-    assert "short_url" in data
-    assert data["short_url"].startswith("http://localhost:8000/")
+# # 데이터베이스 초기화 함수
+# @pytest.fixture(autouse=True)
+# def setup_and_teardown():
+#     metadata.create_all(engine_test)
+#     yield
+#     metadata.drop_all(engine_test)
 
 
-# 기존 단축 URL 반환 테스트
-def test_return_existing_short_url():
-    session = next(get_test_session())
-    service = URLService(session)
-    short_key = service.create_short_url("https://www.example.com")
-    response = client.post("/shorten", json={"url": "https://www.example.com"})
-    assert response.status_code == 200
-    data = response.json()
-    assert data["short_url"] == f"http://localhost:8000/{short_key}"
+# # 종속성 주입 함수 대체
+# # app.dependency_overrides[get_session] = get_test_session
 
 
-# Repository의 get_by_key 메서드 테스트
-def test_repository_get_by_key():
-    session = next(get_test_session())
-    repo = URLRepository(session)
-    url = URL(original_url="https://www.example.com", short_key="abc123")
-    repo.add(url)
-    fetched_url = repo.get_by_key("short_key", "abc123")
-    assert fetched_url is not None
-    assert fetched_url.original_url == "https://www.example.com"
+# # 단축 URL 생성 테스트
+# def test_create_short_url():
+#     response = client.post("/shorten", json={"url": "https://www.example.com"})
+#     assert response.status_code == 200
+#     data = response.json()
+#     assert "short_url" in data
+#     assert data["short_url"].startswith("http://0.0.0.0:8000/")
 
 
-# Repository의 add 메서드 테스트
-def test_repository_add():
-    session = next(get_test_session())
-    repo = URLRepository(session)
-    url = URL(original_url="https://www.example.com", short_key="abc123")
-    added_url = repo.add(url)
-    assert added_url.id is not None
-    assert added_url.original_url == "https://www.example.com"
-    assert added_url.short_key == "abc123"
+# # 기존 단축 URL 반환 테스트
+# def test_return_existing_short_url():
+#     session = get_test_session()
+#     repo = SqlAlchemyRepository(session)
+#     short_key = model.generate_short_url("https://www.example.com", repo)
+#     response = client.post("/shorten", json={"url": "https://www.example.com"})
+#     assert response.status_code == 200
+#     data = response.json()
+#     assert data["short_url"] == f"http://0.0.0.0:8000/{short_key}"
 
 
-# Service의 get 메서드 테스트
-def test_service_get():
-    session = next(get_test_session())
-    service = URLService(session)
-    url = URL(original_url="https://www.example.com", short_key="abc123")
-    service.repo.add(url)
-    fetched_url = service.get(short_key="abc123")
-    assert fetched_url is not None
-    assert fetched_url.original_url == "https://www.example.com"
+# # Repository의 get 메서드 테스트
+# def test_repository_get():
+#     session = get_test_session()
+#     repo = SqlAlchemyRepository(session)
+#     url = URL(original_url="https://www.example_1.com", short_key="abc123")
+#     repo.add(url)
+#     fetched_url = repo.get(short_key="abc123")
+#     assert fetched_url is not None
+#     assert fetched_url.original_url == "https://www.example_1.com"
 
 
-# Service의 create_short_url 메서드 테스트
-def test_service_create_short_url():
-    session = next(get_test_session())
-    service = URLService(session)
-    short_key = service.create_short_url("https://www.example.com")
-    assert len(short_key) == 6
-    fetched_url = service.get(short_key=short_key)
-    assert fetched_url is not None
-    assert fetched_url.original_url == "https://www.example.com"
+# # Repository의 add 메서드 테스트
+# def test_repository_add():
+#     session = get_test_session()
+#     repo = SqlAlchemyRepository(session)
+#     url = URL(original_url="https://www.example_1.com", short_key="abc123")
+#     added_url = repo.add(url)
+#     assert added_url.id is not None
+#     assert added_url.original_url == "https://www.example_1.com"
+#     assert added_url.short_key == "abc123"
---
+++ tests/test_services.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+
+import pytest
+from adapters import repository
+from domain import model
+from service_layer import services
+
+
+class FakeRepository(repository.AbstractRepository):
+    def __init__(self, url):
+        self._url = list(url)
+
+    def add(self, **kwargs):
+        url_data = {
+            key: value
+            for key, value in kwargs.items()
+            if key
+            in ("original_url", "short_key", "created_at", "modified_at", "expired_at")
+        }
+        self._url.append(url_data)
+
+    def get(self, **kwargs):
+        for key, value in kwargs.items():
+            if key in ("original_url", "short_key"):
+                url_data = [e for e in self._url if e.get(key) == value]
+                if url_data:
+                    return model.URL(**url_data[0])
+                return None
+        raise ValueError("No valid key found in kwargs")
+
+    def list(self):
+        return self._url
+
+
+class FakeSession:
+    committed = False
+
+    def commit(self):
+        self.committed = True
+
+
+def test_generate_short_key():
+    repo = FakeRepository([])
+    original_url = "https://www.example.com"
+    expired_at = datetime.strptime("2024-12-31", "%Y-%m-%d")
+    short_key = services.generate_short_key(original_url, expired_at, repo)
+    assert short_key is not None
+
+
+def test_generate_short_key_if_expired_at_none():
+    repo = FakeRepository([])
+    original_url = "https://www.example.com"
+    expired_at = None
+    short_key = services.generate_short_key(original_url, expired_at, repo)
+    assert short_key is not None
+
+
+def test_return_short_key_already_exist():
+    repo = FakeRepository([])
+    original_url = "https://www.example.com"
+    short_key = "XYZ123"
+    expired_at = None
+    repo.add(original_url=original_url, short_key=short_key, expired_date=None)
+    short_key_response = services.generate_short_key(original_url, expired_at, repo)
+    assert short_key == short_key_response
+    assert len(repo.list()) == 1
+
+
+def test_get_original_url():
+    repo = FakeRepository([])
+    original_url = "https://www.example.com"
+    short_key = "XYZ123"
+    repo.add(original_url=original_url, short_key=short_key, expired_date=None)
+    original_url_response = services.get_original_url(short_key, repo)
+    assert original_url == original_url_response
+
+
+def test_get_original_url_if_not_exist():
+    repo = FakeRepository([])
+    original_url = "https://www.example.com"
+    short_key = "XYZ123"
+    repo.add(original_url=original_url, short_key=short_key, expired_date=None)
+
+    with pytest.raises(services.URLNotExist):
+        services.get_original_url("ABC123", repo)
