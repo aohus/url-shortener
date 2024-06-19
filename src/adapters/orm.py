351d6cbdffdc68a00e2a599adefd3a4206e1b5b6
aohus <dbtnghk528@gmail.com>
2024-06-19 13:51:29 +0900

    feature: add timestamps

--- src/adapters/orm.py
+++ src/adapters/orm.py
@@ -1,5 +1,5 @@
 from domain import model
-from sqlalchemy import Column, Date, Integer, MetaData, String, Table
+from sqlalchemy import Column, DateTime, Integer, MetaData, String, Table, event, func
 from sqlalchemy.orm import mapper
 
 metadata = MetaData()
@@ -10,12 +10,17 @@ url = Table(
     Column("id", Integer, primary_key=True, autoincrement=True),
     Column("original_url", String, index=True),
     Column("short_key", String, index=True),
-    # Column("create_at", Date),
+    Column("created_at", DateTime, default=func.now(), nullable=False),
+    Column(
+        "modified_at", DateTime, default=func.now(), onupdate=func.now(), nullable=False
+    ),
+    Column("expired_at", DateTime, nullable=True),
 )
 
 
 def start_mappers():
-    mapper(
-        model.URL,
-        url,
-    )
+    mapper(model.URL, url)
+
+    @event.listens_for(model.URL, "before_update")
+    def receive_before_update(mapper, connection, target):
+        target.update_modified_at()
