7e6674121b734b8530c1a713a4b380d232ab0ef6
aohus <dbtnghk528@gmail.com>
2024-06-19 14:15:59 +0900

    fix: url table schema condition

--- src/adapters/orm.py
+++ src/adapters/orm.py
@@ -8,8 +8,8 @@ url = Table(
     "url",
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("original_url", String, index=True),
-    Column("short_key", String, index=True),
+    Column("original_url", String, index=True, nullable=False),
+    Column("short_key", String, index=True, nullable=False),
     Column("created_at", DateTime, default=func.now(), nullable=False),
     Column(
         "modified_at", DateTime, default=func.now(), onupdate=func.now(), nullable=False
